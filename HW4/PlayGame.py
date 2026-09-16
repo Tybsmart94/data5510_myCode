cacfe62fa623ad2b077f7505cc1df30c4910026e
Ty Smart <tybsmart@icloud.com>
2026-09-16 03:38:41 +0000

    Worked on homework

--- HW4/PlayGame.py
+++ HW4/PlayGame.py
@@ -4,26 +4,50 @@ print("Welcome to BlackJack!")
 deck = DeckOfCards()
 deck.shuffle_deck()
 score = 0
+hand = []
+dealer_Hand = []
 
+#Initial Deal
+card = deck.get_card()
+hand.append(card.val)
+card2 = deck.get_card()
+hand.append(card2.val)
+
+dealer = deck.get_card()
+dealer_Hand.append(dealer.val)
+dealer2 = deck.get_card()
+dealer_Hand.append(dealer2.val)
+
+
+print("Your Hand")
+print(f"{card}, {card2}")
+print(hand)
+print()
+
+print("Dealer hand")
+print(f"{dealer}")
+
+score += card.val
+score += card2.val
+print(f"Your score is {score}")
+print()
 
 while score < 21:
-    #Initial Deal
-    card = deck.get_card()
-    card2 = deck.get_card()
-
-    print(card, card2)
-
-    score += card.val
-    score += card2.val
-
-    print(f"Your score is {score}")
-
     #Game loop
     user_Input = input("Would you like to hit? (y/n): ")
 
     if user_Input == 'y':
-        card3 = deck.get_card()
-        score += card3.val
+        new_Card = deck.get_card()
+        if score >= 11 and new_Card.val == 11:
+            new_Card.val = 1
+        print()
+        print(f"{card}, {card2}, {new_Card}")
+        score += new_Card.val
+        hand.append(new_Card.val)
+        print(f"Your score is {score}")
+        print()
+        print(hand)
+        print()
 
         if score < 21:
             print(f"Your score is {score}")
@@ -37,5 +61,6 @@ while score < 21:
             print("BLACKJACK")
             break
     else:
-        print(f"Your score is {score}, wimp")
-        break
+        print(f"{dealer}, {dealer2}")
+        dealer3 = deck.get_card()
+        dealer_Hand.append(dealer3.val)
