790b6638c42616671ba702eeb9de224d951ac9c3
Ty Smart <tybsmart@icloud.com>
2026-09-17 20:28:42 +0000

    Tweaked a few things

--- HW4/PlayGame.py
+++ HW4/PlayGame.py
@@ -1,10 +1,14 @@
+#Imports and marker variable
 from DeckOfCards import *
 playing = True
 
+#Welcome message only to be played once
+print("Welcome to BlackJack!")
+print()
+
+#Entire game loop
 while playing == True:
-    #Welcomes player and initializes deck
-    print("Welcome to BlackJack!")
-    print()
+    #Initialization of deck
     deck = DeckOfCards()
 
     #Prints unshuffled and shuffled decks
@@ -15,7 +19,7 @@ while playing == True:
     print("Here is the shuffled deck")
     deck.print_deck()
 
-    #initializes scores and busted statement
+    #initializes scores and busted variable
     print()
     score = 0
     dealer_Score = 0
@@ -28,17 +32,22 @@ while playing == True:
     dealer = deck.get_card()
     dealer2 = deck.get_card()
 
-    #Shows player their cards
+    #Shows player their cards and the dealer's first card
     print(f"Card number 1 is: {card}")
     print(f"Card number 2 is: {card2}")
     print()
+    print(f"The dealers first card is {dealer}")
+    print()
 
-    #Sets scores and shows player their score
+    #Sets scores
     dealer_Score += dealer.val
     dealer_Score += dealer2.val
 
     score += card.val
     score += card2.val
+
+    #Counter variable
+    card_Num_Player = 2
 
     #Ends game if player gets blackjack
     if score == 21 and dealer_Score != 21:
@@ -64,33 +73,39 @@ while playing == True:
             print("Thanks for playing!")
             playing == False
             break
+    
+    #Shows you your score
     print(f"Your total score is {score}")
 
     #Players game loop
     while busted == False:
+        card_Num_Player += 1
+
         user_Input = input("Would you like to hit? (y/n): ")
 
         #Hit game loop
         if user_Input == 'y':
             new_Card = deck.get_card()
-            #Changes aces to ones if needed
-            if score >= 11 and card.val == 11 and score != 21:
-                # print(card.val, "This worked player 1")
-                card.val = 1
-                score -= 10
-            if score >= 11 and card2.val == 11 and score != 21:
-                # print(card2.val, "This worked player 2")
-                card2.val = 1
-                score -= 10
-            if score >= 11 and new_Card.val == 11 and score != 21:
-                # print(new_Card.val, "This worked player new")
-                new_Card.val = 1
-            print()
-            print(f"Card number 3 is: {new_Card}")
             score += new_Card.val
+
+            #Changes aces to ones if needed
+            if score > 21 and card.val == 11:
+                # print(card.val, "This worked player 1")
+                score -= 10
+            if score > 21 and card2.val == 11:
+                # print(card2.val, "This worked player 2")
+                score -= 10
+            if score > 21 and new_Card.val == 11:
+                # print(new_Card.val, "This worked player new")
+                score -= 10
+            
+            #Updates the player on their standing
+            print()
+            print(f"Card number {card_Num_Player} is: {new_Card}")
             print(f"Your total score is {score}")
             print()
 
+            #Checks score to see if player can keep going or should move on
             if score < 21:
                 continue
             elif score > 21:
@@ -102,18 +117,20 @@ while playing == True:
                 print(f"Your score is {score}")
                 # print("BLACKJACK")
                 break
+        
+        #Ends players loop so dealer can start
         else:
             break
 
-
     #Dealers game loop
-    print()
-    print("OKAY, dealers turn")
+    print("Dealers turn")
     print()
     print(f"Dealer card number 1: {dealer}")
     print(f"Dealer card number 2: {dealer2}")
     print(f"Dealer's score is {dealer_Score}")
     print()
+
+    #Counter variable
     card_Num = 2
 
     #Dealers hit loop
@@ -124,45 +141,55 @@ while playing == True:
         if busted == True and dealer_Score < 21:
             print("Dealer wins")
             break
-        if dealer_Score > score and dealer_Score < 21:
+        #Makes sure that the dealer won't continue playing if it is closer to 21 than us
+        elif dealer_Score > score and dealer_Score < 21:
             print("You lost, dealer won")
             break
+        #Actual hit loop
         else:
             new_Dealer = deck.get_card()
+            dealer_Score += new_Dealer.val
+
+            #To change aces to ones if needed
+            if dealer_Score > 21 and dealer.val == 11:
+                # print(dealer.val, "This worked")
+                dealer_Score -= 10
+            if dealer_Score > 21 and dealer2.val == 11:
+                # print(dealer2.val, "This worked 2")
+                dealer_Score -= 10
+            if dealer_Score > 21 and new_Dealer.val == 11:
+                # print(new_Dealer.val, "This worked new_Dealer")
+                dealer_Score -= 10
+
+            #Updates player on dealer's standing
             print(f"Dealer card number {card_Num}: {new_Dealer}")
             print()
-            #To change aces to ones if needed
-            if dealer_Score >= 11 and dealer.val == 11 and dealer_Score != 21:
-                # print(dealer.val, "This worked")
-                dealer.val = 1
-                dealer_Score -= 10
-            if dealer_Score >= 11 and dealer2.val == 11 and dealer_Score != 21:
-                # print(dealer2.val, "This worked 2")
-                dealer2.val = 1
-                dealer_Score -= 10
-            if dealer_Score >= 11 and new_Dealer.val == 11 and dealer_Score != 21:
-                # print(new_Dealer.val, "This worked new_Dealer")
-                new_Dealer.val = 1
-            dealer_Score += new_Dealer.val
             print(f"Dealer's score is {dealer_Score}")
+
+            #Checks scores to see if dealer can continue
             if dealer_Score < 21 and dealer_Score < score:
                 continue
             elif dealer_Score < 21 and dealer_Score > 17:
                 if score > dealer_Score:
-                    print("YOU WIN because dealer went over you")
+                    print("YOU WIN because you were closer to 21")
                     break
                 elif dealer_Score == score:
-                    print("WOAH, that's a push")
+                    print("Push")
                     break
                 else:
-                    print("You lose because dealer was better")
+                    print("You lose because dealer was closer to 21")
                     break
+            
+            #Special conditions + Most common condition in else
             elif dealer_Score == 21 and score < 21:
                 print("DEALER GOT BLACKJACK, YOU SHOULD'VE HIT!")
+                break
             elif dealer_Score == 21 and score > 21:
                 print("DEALER GOT BLACKJACK, YOU BUSTED!")
+                break
             elif dealer_Score == 21 and score == 21:
                 print("YOU BOTH GOT BLACKJACK??? Crazy")
+                break
             else:
                 print("Dealer busted, you win")
                 break
